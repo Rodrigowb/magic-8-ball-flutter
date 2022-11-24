43f923a476d28ea8e6e7c4607bdc576387e5569b
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-11-24 09:37:47 -0300

    crating astatefullwidget and a function to generate random numbers to change the image after clicking it. Using setState to change the state of the widget and reflects in the UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          title: Text('Ask Whatever You Want'),
+          backgroundColor: Colors.blue,
+        ),
+        body: AskApp(),
+      ),
+    ),
+  );
+}
+
+class AskApp extends StatefulWidget {
+
+  @override
+  State<AskApp> createState() => _AskAppState();
+}
+
+class _AskAppState extends State<AskApp> {
+
+  int imageNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+
+    void changeNumber(){
+      imageNumber = Random().nextInt(5) + 1;
+    }
+
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Expanded(
+          child: Container(
+            child: TextButton(
+              child: Image.asset('images/ball$imageNumber.png'),
+              onPressed: (){
+                setState(() {
+                  changeNumber();
+                });
+              }
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
